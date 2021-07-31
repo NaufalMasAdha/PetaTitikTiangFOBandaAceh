87631aa0719b76f9f3475d78efc17045d636cb50
farhanka <farhan10@mhs.unsyiah.ac.id>
2021-07-31 11:28:38 +0700

    disable guest registration

---
+++ app/Http/Controllers/CustomAuthController.php
@@ -0,0 +1,87 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Hash;
+use Session;
+use App\Models\User;
+use Illuminate\Support\Facades\Auth;
+
+class CustomAuthController extends Controller
+{
+
+    public function index()
+    {
+        return view('auth.login');
+    }  
+      
+
+    public function customLogin(Request $request)
+    {
+        $request->validate([
+            'email' => 'required',
+            'password' => 'required',
+        ]);
+   
+        $credentials = $request->only('email', 'password');
+        if (Auth::attempt($credentials)) {
+            return redirect()->intended('dashborad')
+                        ->withSuccess('Berhasil Login!');
+        }
+  
+        return redirect("login")->withSuccess('Kombinasi email dan kata sandi tidak ditemukan');
+    }
+
+
+
+    public function registration()
+    {
+        return view('auth.registration');
+    }
+      
+
+    public function customRegistration(Request $request)
+    {  
+        $request->validate([
+            'name' => 'required',
+            'email' => 'required|email|unique:users',
+            'role' => 'required',
+            'password' => 'required|min:6',
+        ]);
+           
+        $data = $request->all();
+        $check = $this->create($data);
+         
+        return redirect("dashboard")->withSuccess('Anda berhasil login');
+    }
+
+
+    public function create(array $data)
+    {
+      return User::create([
+        'name' => $data['name'],
+        'email' => $data['email'],
+        'role' => $data['role'],
+        'password' => Hash::make($data['password'])
+      ]);
+    }    
+    
+
+    public function dashboard()
+    {
+        if(Auth::check()){
+            return view('dashboard');
+        }
+  
+        return redirect("login")->withSuccess('Login dulu');
+    }
+    
+
+    public function signOut() {
+        Session::flush();
+        Auth::logout();
+  
+        return Redirect('login');
+    }
+}
