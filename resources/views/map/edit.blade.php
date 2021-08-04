931ce7a4af0c4f48c0d325779a59b7a280426084
farhanka <farhan10@mhs.unsyiah.ac.id>
2021-08-04 18:05:35 +0700

    Buat routing halaman

---
+++ resources/views/map/edit.blade.php
@@ -0,0 +1,22 @@
+@extends('layouts.app')
+<!--  -->
+@section('title','FO Maps | Edit Tiang FO') @section('subtitle', "Edit Tiang
+FO") @section('nav-menu')
+<div class="nav_list">
+  <a href="{{ route('daftar_tiang') }}" class="nav_link active">
+    <i class="bx bx-current-location nav_icon"></i>
+    <span class="nav_name">Daftar Tiang FO</span>
+  </a>
+</div>
+<div class="nav_list">
+  <a href="{{ route('daftar_instansi') }}" class="nav_link">
+    <i class="bx bx-buildings nav_icon"></i>
+    <span class="nav_name">Daftar Instansi</span>
+  </a>
+</div>
+@endsection
+<!--  -->
+
+@section('content')
+<h1>Disini nanti ada map</h1>
+@endsection
