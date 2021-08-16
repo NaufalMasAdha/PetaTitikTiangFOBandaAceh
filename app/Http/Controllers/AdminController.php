47aae386ec893addc2883099365b50b0b27c10a0
farhanka <farhan10@mhs.unsyiah.ac.id>
2021-08-16 09:06:30 +0700

    update  validasi nama

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -21,7 +21,7 @@ class AdminController extends Controller
     public function store(Request $request)
     {
         $request->validate([
-            'name' =>  'required|alpha',
+            'name' =>  'required',
             'email' => 'required|unique:users',
             'role' =>'required'
             
